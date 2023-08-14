57d28f9b81f5da2e727cca120435e7e6f5778b85
tracy macaulay-higgan <tracy@mad.geek.nz>
2023-08-14 21:59:34 +1200

    lost-line threshold seems to be working

--- Line_following/Line_following.ino
+++ Line_following/Line_following.ino
@@ -5,14 +5,12 @@
 #include <Wire.h>
 #include <ZumoShield.h>
 
-
-
 ZumoReflectanceSensorArray reflectanceSensors;
 ZumoMotors motors;
 int lastError = 0;
 
 const int TOP_SPEED = 100;
-const int LINE_THRESHOLD = 500;
+const int LINE_THRESHOLD = 100;
 const int SEARCH_DURATION = 1000;
 
 enum RobotState
@@ -25,19 +23,52 @@ RobotState currentState = FIND_PATH;
 
 void setup()
 {
+  Serial.begin(9600);
   reflectanceSensors.init();
   calibration();
 }
 
 bool hasLostLine(unsigned int sensors[6])
 {
-  if (sensors[6] < LINE_THRESHOLD)
+  if (sensors[0] < LINE_THRESHOLD 
+      && sensors[1] < LINE_THRESHOLD 
+      && sensors[2] < LINE_THRESHOLD 
+      && sensors[3] < LINE_THRESHOLD 
+      && sensors[4] < LINE_THRESHOLD 
+      && sensors[5] < LINE_THRESHOLD)
   {
-    return true; // Robot has lost the line
+    return true;
   }
   else
   {
-    return false; // Robot has not lost the line
+    return false;
+  }
+}
+
+void output(unsigned int sensors[6])
+{
+  static uint16_t lastSampleTime = 0;
+
+  if ((uint16_t)(millis() - lastSampleTime) >= 100)
+  {
+    lastSampleTime = millis();
+
+    // // Read the reflectance sensors.
+    // uint16_t sensorValues[6]
+    // reflectanceSensors.read(sensorValues, QTR_EMITTERS_ON);
+
+    // Send the results to the serial monitor.
+    char buffer[80];
+    sprintf(buffer, "%4d %4d %4d %4d %4d %4d %c\n",
+      sensors[0],
+      sensors[1],
+      sensors[2],
+      sensors[3],
+      sensors[4],
+      sensors[5],
+      'E'
+    );
+    Serial.print(buffer);
   }
 }
 
@@ -45,6 +76,8 @@ void loop()
 {
   unsigned int sensors[6];
   int position = reflectanceSensors.readLine(sensors);
+
+  output(sensors);
 
   switch (currentState)
   {
@@ -82,7 +115,7 @@ void calibration()
   digitalWrite(13, HIGH);
 
   // Wait for set time to begin calibration in this case it is 1 second
- 
+
   delay(1000);
   int i;
   for (i = 0; i < 150; i++)
@@ -113,26 +146,23 @@ void follow()
 
   unsigned int sensors[6];
 
-
   // position of line and sensors
   int position = reflectanceSensors.readLine(sensors);
 
-
-  // this is how far away from the centre of the line the robot is 
+  // this is how far away from the centre of the line the robot is
   int error = position - 2500;
 
-  // motor speed difference for the motors 
+  // motor speed difference for the motors
   int speedDifference = error / 4 + 6 * (error - lastError);
 
   lastError = error;
 
-  //individual motor speeds + turns left - turns right
-  
+  // individual motor speeds + turns left - turns right
+
   int m1Speed = TOP_SPEED + speedDifference;
   int m2Speed = TOP_SPEED - speedDifference;
 
- 
-// Setting motor speeds and setting foward and backwards
+  // Setting motor speeds and setting foward and backwards
 
   if (m1Speed < -TOP_SPEED)
     m1Speed = -TOP_SPEED;
@@ -157,7 +187,7 @@ void searchForLine()
 
   reverse(1000);
 
-   digitalWrite(13, LOW);
+  digitalWrite(13, LOW);
 }
 
 void straight(int duration)
