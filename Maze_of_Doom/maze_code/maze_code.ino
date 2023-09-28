202f9356f3590c94293d5f58694103336e232b37
tracy macaulay-higgan <tracy@mad.geek.nz>
2023-09-28 21:45:30 +1300

    threshold added for lights to start motors

--- Maze_of_Doom/maze_code/maze_code.ino
+++ Maze_of_Doom/maze_code/maze_code.ino
@@ -10,65 +10,65 @@ ZumoMotors motors;
 
 // Define LDR pin and motor pins
 
-const int leftMotorPin = 5;  // Left motor control pin
-const int rightMotorPin = 4; // Right motor control pin
 
+  const int left = A5;
+  const int right = A4;
 
-const int lightThreshold = 40;
+  int value;
+  int value1;
 
-// Define motor control functions
-void forward() {
-  digitalWrite(leftMotorPin, HIGH);
-  digitalWrite(rightMotorPin, HIGH);
-   motors.setLeftSpeed(200);   
-  motors.setRightSpeed(200);  
+const int lightThreshold = 60;
+const int TOP_SPEED = 250;
 
+void straight(int duration)
+{
+  motors.setLeftSpeed(TOP_SPEED);
+  motors.setRightSpeed(TOP_SPEED);
+  delay(duration);
 }
 
-void left() {
-  digitalWrite(leftMotorPin, LOW);
-  digitalWrite(rightMotorPin, HIGH);
-  motors.setLeftSpeed(-200);   
-  motors.setRightSpeed(200); 
-
-
+void reverse(int duration)
+{
+  motors.setLeftSpeed(-TOP_SPEED);
+  motors.setRightSpeed(-TOP_SPEED);
+  delay(duration);
 }
 
-void right() {
-  digitalWrite(leftMotorPin, HIGH);
-  digitalWrite(rightMotorPin, LOW);
-motors.setLeftSpeed(200);
-  motors.setRightSpeed(-200);
-  
-
+void turnLeft(int duration)
+{
+  motors.setLeftSpeed(-TOP_SPEED);
+  motors.setRightSpeed(TOP_SPEED);
+  delay(duration);
 }
 
-void backward() {
-  digitalWrite(leftMotorPin, LOW);
-  digitalWrite(rightMotorPin, LOW);
+void turnRight(int duration)
+{
+  motors.setLeftSpeed(TOP_SPEED);
+  motors.setRightSpeed(-TOP_SPEED);
+  delay(duration);
 }
 
 void setup() {
   Serial.begin(9600);
   // Initialize motor control pins as OUTPUT
-  pinMode(leftMotorPin, OUTPUT);
-  pinMode(rightMotorPin, OUTPUT);
+  pinMode(left, OUTPUT);
+  pinMode(right, OUTPUT);
 
 }
 
 
 void loop() {
   // Read the LDR sensor value
-  int lightValue = analogRead(leftMotorPin);
-  Serial.println(leftMotorPin);
-  delay(2000);
-  int lightValue1 = analogRead(rightMotorPin);
-  Serial.println(rightMotorPin);
-  delay(2000);
+  int value = analogRead(left);
+  Serial.println(value);
+  delay(200);
+  int value1 = analogRead(right);
+  Serial.println(value1);
+  delay(200);
 
   // Check if light is detected
-  if (lightValue > lightThreshold || lightValue1 > lightThreshold) {
-    forward(); // Move forward towards the light
+  if (value > lightThreshold || value1 > lightThreshold) {
+    straight(100); // Move forward towards the light
   } else {
     // // No light detected, make a decision (you can customize this logic)
     // int randomDecision = random(4); // Generate a random decision (0-3)
@@ -76,10 +76,10 @@ void loop() {
     // // Perform different actions based on the random decision
     // switch (randomDecision) {
     //   case 0:
-    //     left(); // Turn left
+    //     left1(); // Turn left
     //     break;
     //   case 1:
-    //     right(); // Turn right
+    //     right2(); // Turn right
     //     break;
     //   case 2:
     //     backward(); // Move backward
@@ -87,7 +87,8 @@ void loop() {
     //   default:
     //     forward(); // Default to moving forward
     //     break;
-    }
+    // }
   }
 }
+
 
